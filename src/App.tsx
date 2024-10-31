a236b399dc72d9c398430cf3bb399a55d65fe28f
Rafael Peral Venet Ferreira <rafaelvenet@gmail.com>
2024-10-31 20:40:38 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import WelcomeSection from "./components/sections/welcome/welcome_section"
+import AboutMeSection from "./components/sections/about-me/about-me_section"
+import SkilsSection from "./components/sections/skils/skils_section"
+
 export default function App() {
   return (
-    <div className="flex items-center justify-center">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore laudantium accusamus fugiat! Officiis corrupti facere dolor pariatur magnam veritatis, aspernatur minus et beatae harum, temporibus voluptates amet ab earum architecto!Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore laudantium accusamus fugiat! Officiis corrupti facere dolor pariatur magnam veritatis, aspernatur minus et beatae harum, temporibus voluptates amet ab earum architecto!Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore laudantium accusamus fugiat! Officiis corrupti facere dolor pariatur magnam veritatis, aspernatur minus et beatae harum, temporibus voluptates amet ab earum architecto!Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore laudantium accusamus fugiat! Officiis corrupti facere dolor pariatur magnam veritatis, aspernatur minus et beatae harum, temporibus voluptates amet ab earum architecto!Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore laudantium accusamus fugiat! Officiis corrupti facere dolor pariatur magnam veritatis, aspernatur minus et beatae harum, temporibus voluptates amet ab earum architecto!Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore laudantium accusamus fugiat! Officiis corrupti facere dolor pariatur magnam veritatis, aspernatur minus et beatae harum, temporibus voluptates amet ab earum architecto!Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore laudantium accusamus fugiat! Officiis corrupti facere dolor pariatur magnam veritatis, aspernatur minus et beatae harum, temporibus voluptates amet ab earum architecto!
+    <div className="flex flex-col items-center justify-center">
+      <WelcomeSection/>
+      <AboutMeSection/>
+      <SkilsSection/>
     </div>
   )
 }
