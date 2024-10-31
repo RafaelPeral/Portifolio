7fa5407ef7a857b46d4c17d7c4b78f0a0a7aa234
Rafael Peral Venet Ferreira <rafaelvenet@gmail.com>
2024-10-31 12:41:19 -0300

    adding footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,7 +1,16 @@
 export default function Footer() {
+    const date = new Date();
+    const year = date.getFullYear();
     return (
-        <div>
-            <p>Footer</p>
+        <div className="w-full bg-secondary flex items-center justify-center gap-60 mt-10">
+            <div className="flex flex-col gap-2">
+                <p>Created By</p>
+                <p>Rafael Peral</p>
+            </div>
+            <div className="flex flex-col gap-2">
+                <p>&copy;{year} - Copyright</p>
+                <p>All Rights reserved</p>
+            </div>
         </div>
     )
 }
