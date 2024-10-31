f524f48a7d9f06098a6e02d7bc223348e39c56ba
Rafael Peral Venet Ferreira <rafaelvenet@gmail.com>
2024-10-31 20:06:11 -0300

    welcome section

---
+++ src/components/welcome/welcome.tsx
@@ -0,0 +1,23 @@
+import { github_url, gif_computer_url } from '@/utils/url'
+
+export default function Welcome() {
+    return (
+        <section className="max-w-screen-xl mx-auto h-headerscreen flex items-center justify-center md:gap-32 lg:gap-64 py-24" id="welcome">
+            <div className="h-full w-1/2 flex flex-col items-center justify-center gap-6">
+                <h1 className=' text-5xl font-bold tx-violet'>Rafael Peral</h1>
+                <h2 className=' text-lg'>Your Full Stack Developer.</h2>
+                <h2 className=' text-lg text-gray-300'>Building the future, one line of code at a time.</h2>
+                <a 
+                    className=' flex justify-center text-white hover:text-gray-400 duration-500 p-3 font-bold bg-gradient-to-r from-dark-purple to-bright-purple rounded w-44'
+                    href={github_url}
+                    target="_blank"
+                >
+                    Visit GitHub ➝
+                </a>
+            </div>
+            <div className='hidden w-1/2 lg:flex'>
+                <img src={gif_computer_url} alt="Gif Computer" className="w-full"/>
+            </div>
+      </section>
+    )
+}
--- src/utils/url.tsx
+++ src/utils/url.tsx
@@ -9,3 +9,5 @@ export const telegram_url = `https://t.me/RafaelPeral`
 export const becons_url = `https://beacons.ai/rafael_peral`
 
 export const avatar_url = `https://github.com/RafaelPeral.png`
+
+export const gif_computer_url = `https://github.com/user-attachments/assets/e99790da-6072-4582-8ea3-36c27df5896b`
