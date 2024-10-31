760a26c65c5bbd2dd310542d2f5a543146bb40d0
Rafael Peral Venet Ferreira <rafaelvenet@gmail.com>
2024-10-31 20:50:00 -0300

    refactoring

--- src/components/sections/skils/back.tsx
+++ src/components/sections/skils/back.tsx
@@ -7,28 +7,35 @@ interface BackProps {
 }
 
 export default function Back( { setName, setDescription, name } : BackProps ) {
+    const python = {
+        name: "Python",
+        descricao: "Python is an interpreted, object-oriented, high-level programming language with dynamic semantics. Being used for server-side web development, software development and system scripts.",
+        caminho : "/skills/hard/python.png"
+    }
+    const django = {
+        name: "Django",
+        descricao: "Django is a web framework for Python that encourages rapid development and clean, pragmatic design. He is responsible for much of the web development work.",
+        caminho : "/skills/hard/django.png"
+    }
+
+    const tecnologias = [python, django]
+
     return (
         <div className="flex flex-col items-center mr-10">
             <div className="mb-4">
                 <h2 className=' text-2xl font-bold'>Back-end</h2>
                 <div className=" w-8 h-[2px] bg-bright-purple"/>
             </div>
-            <Skill 
-                nome="Python" 
-                descricao='Python is an interpreted, object-oriented, high-level programming language with dynamic semantics. Being used for server-side web development, software development and system scripts.' 
-                nomeFuncao={setName} 
-                descricaoFuncao={setDescription} 
-                nomeVar={name}
-                caminho="/skills/hard/python.png" 
-            />
-            <Skill
-                nome="Django"
-                descricao="Django is a web framework for Python that encourages rapid development and clean, pragmatic design. He is responsible for much of the web development work."
-                nomeFuncao={setName} 
-                descricaoFuncao={setDescription} 
-                nomeVar={name}
-                caminho="/skills/hard/django.png" 
-            />
+            {tecnologias.map((tecnologia) => (
+                <Skill 
+                    nome={tecnologia.name}
+                    descricao={tecnologia.descricao}
+                    nomeFuncao={setName} 
+                    descricaoFuncao={setDescription} 
+                    nomeVar={name}
+                    caminho={tecnologia.caminho}
+                />
+            ))}
         </div>
     )
 }
--- src/components/sections/skils/front.tsx
+++ src/components/sections/skils/front.tsx
@@ -7,44 +7,45 @@ interface FrontProps {
 }
 
 export default function Front( { setName, setDescription, name } : FrontProps ) {
+    const react = {
+        name: "React",
+        descricao: "React is an open source JavaScript library focused on creating user interfaces on web pages. This framework also allows the creation of reusable components for our application and the composition of elements that can be changed, without the need to reload the page.",
+        caminho : ""
+    }
+    const typescript = {
+        name: "TypeScript",
+        descricao: "TypeScript is a programming language developed and maintained by Microsoft. It is a strict syntactical superset of JavaScript, and adds optional static typing to the language.",
+        caminho : ""
+    }
+    const javascript = {
+        name: "JavaScript",
+        descricao: "JavaScript is a lightweight, interpreted, high-level, dynamic, and interpreted programming language.",
+        caminho : ""
+    }
+    const tailwind = {
+        name: "Tailwind CSS",
+        descricao: "Tailwind CSS is a utility-first CSS framework for rapidly building custom user interfaces.",
+        caminho : ""
+    }
+
+    const tecnologias = [react, typescript, javascript, tailwind];
+
     return (
         <div className="flex flex-col items-center mr-10">
             <div className="mb-4">
                 <h2 className='text-2xl font-bold'>Front-end</h2>
                 <div className="w-8 h-[2px] bg-bright-purple"/>
             </div>
-            <Skill
-                nome="React"
-                descricao="React is an open source JavaScript library focused on creating user interfaces on web pages. This framework also allows the creation of reusable components for our application and the composition of elements that can be changed, without the need to reload the page."
-                nomeFuncao={setName}
-                descricaoFuncao={setDescription}
-                nomeVar={name}
-                caminho="/skills/hard/react.png" 
-            />
-            <Skill
-                nome="TypeScript"
-                descricao="TypeScript is a programming language developed and maintained by Microsoft. Introducing additional features such as strict type binding for JavaScript. TypeScript overcomes one of JavaScript's biggest flaws, which is finding bugs in the code during the development phase, checking for any problems at compile time, preventing the delivery of the defective product to the end customer."
-                nomeFuncao={setName}
-                descricaoFuncao={setDescription}
-                nomeVar={name}
-                caminho="/skills/hard/typescript.png" 
-            />
-            <Skill
-                nome="JavaScript"
-                descricao="JavaScript is a lightweight language commonly used on web pages, whose implementations allow client-side scripting to interact with the user and create dynamic pages. It is an interpreted programming language with object-oriented features."
-                nomeFuncao={setName}
-                descricaoFuncao={setDescription}
-                nomeVar={name}
-                caminho="/skills/hard/javascript.png" 
-            />
-            <Skill
-                nome="Tailwind CSS"
-                descricao="Tailwind CSS is a utility CSS framework that offers predefined classes to quickly style HTML elements. Instead of providing pre-styled components, it allows you to combine classes to create custom, responsive designs."
-                nomeFuncao={setName}
-                descricaoFuncao={setDescription}
-                nomeVar={name}
-                caminho="/skills/hard/tailwindcss.png" 
-            />
+            {tecnologias.map((tecnologia) => (
+                <Skill
+                    nome={tecnologia.name}
+                    descricao={tecnologia.descricao}
+                    nomeFuncao={setName}
+                    descricaoFuncao={setDescription}
+                    nomeVar={name}
+                    caminho={tecnologia.caminho}
+                />
+            ))}
         </div>
     )
 }
