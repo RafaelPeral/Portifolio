64b94e58bb204f7050496171c9077c3d7e1d211d
Rafael Peral Venet Ferreira <rafaelvenet@gmail.com>
2024-11-01 03:57:50 -0300

    Update about-me_section.tsx

--- src/components/sections/about-me/about-me_section.tsx
+++ src/components/sections/about-me/about-me_section.tsx
@@ -1,4 +1,5 @@
 import { instagram_url, avatar_url } from '@/utils/url'
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
 
 export default function AboutMeSection() {
     return (
@@ -16,9 +17,38 @@ export default function AboutMeSection() {
                 <p className='text-muted-foreground text-left'>
                     I work as a Full Stack developer, with expertise in Python and Flesk for back-end development and React with TypeScript for front-end development.
                 </p>
-                <button className='hover:text-gray-400 duration-500 p-3 font-bold bg-gradient-violet w-44'>
-                    More about me ➝
-                </button>
+                <Popover>
+                    <PopoverTrigger>
+                        <p className='hover:text-gray-400 duration-500 p-3 font-bold bg-gradient-violet w-44'>
+                            More about me ➝
+                        </p>
+                    </PopoverTrigger>
+                    <PopoverContent className="p-6 max-w-xl mx-auto bg-primary shadow-md rounded-lg z-0 md:w-[500px] lg:">
+                        <h2 className="text-2xl font-bold text-center mb-4 text-primary-foreground">💫 About Me</h2>
+                        <details className="border border-gray-300 rounded-lg p-4">
+                            <summary className="text-lg cursor-pointer text-muted">
+                                Hello! I am a Full Stack developer from Brazil, currently living in São Paulo, with a passionate drive to create solutions that truly make a difference in people's lives. My journey in programming began with an insatiable curiosity for technology, and since then, I have been dedicated to continually enhancing my skills and knowledge. I believe in the power of technology to transform ideas into reality and am excited to share my journey with you.
+                            </summary>
+
+                            <div className="mt-4 space-y-4">
+                                <h3 className="text-xl font-semibold mt-4 text-primary-foreground">🚀 Continuous Learning</h3>
+                                <p className='text-muted-foreground font-semibold'>
+                                    Currently, I am exploring the fascinating world of software architecture, seeking to understand how to structure applications efficiently and scalably, always aiming to create high-quality systems.
+                                </p>
+
+                                <h3 className="text-xl font-semibold mt-4 text-primary-foreground">🌱 Work Philosophy</h3>
+                                <p className='text-muted-foreground font-semibold'>
+                                    I firmly believe that technology should be an ally in solving real problems. My purpose is to apply my programming knowledge to develop tools that genuinely impact people's everyday lives. I am always open to new challenges and learning opportunities.
+                                </p>
+
+                                <h3 className="text-xl font-semibold mt-4 text-primary-foreground">🤝 Let's Connect</h3>
+                                <p className='text-muted-foreground font-semibold'>
+                                    I am eager to meet people who share similar interests and collaborate on innovative projects. If you have an exciting idea or opportunity, feel free to reach out. Let's work together to turn ideas into reality!
+                                </p>
+                            </div>
+                        </details>
+                    </PopoverContent>
+                </Popover>
             </div>
         </section>
     )
